20099aa87819e0fad9ef8cc01fdbfeb6db9af2bf
thanhantran1 <thanhantran21@gmail.com>
2021-11-19 13:03:11 +0700

    Featured Movie on client side

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Featured from '../../components/featured/Featured';
 import NavBar from '../../components/navBar/NavBar';
 import './Home.scss';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div className="home">
             <NavBar />
+            <Featured />
         </div>
     )
 }
