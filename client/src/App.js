987c96ba4ee4cf26133659baad2e236f04caa5f9
thanhantran1 <thanhantran21@gmail.com>
2021-11-19 17:06:29 +0700

    React register page design on client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import './App.scss';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Watch />
+      {/* <Watch /> */}
+      <Register />
     </div>
   );
 }
