1d2a4697cc123341bfba662a74e6773f5d3e6dba
thanhantran1 <thanhantran21@gmail.com>
2021-11-19 17:31:01 +0700

    React login page on client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import './App.scss';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <Watch /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
