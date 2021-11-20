0d0400c8a521db993514ca4cd332d299bffb231e
thanhantran1 <thanhantran21@gmail.com>
2021-11-20 12:17:14 +0700

    Node.js MongoDB Connection on server side

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@movie.1clg4.mongodb.net/movie?retryWrites=true&w=majority`,
+            {
+                useCreateIndex: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false
+            }
+        )
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+} 
+
+connectDB();
 
 const port = 5000
 app.listen(port, () => {
