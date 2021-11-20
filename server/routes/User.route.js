b44bbce2c5563e573bf6f4b6d618fb523f1c85bd
thanhantran1 <thanhantran21@gmail.com>
2021-11-20 16:36:47 +0700

    Add function get all user on server side

--- server/routes/User.route.js
+++ server/routes/User.route.js
@@ -62,4 +62,23 @@ router.get('/find/:id', async (req, res) => {
     }
 });
 
+// @router api/users/?new=true
+// @desc GET all user
+// access Private
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query
+                ? await User.find().sort({ _id: -1 }).limit(5)
+                : await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+      res.status(403).json("You are not allowed to see all users!");
+    }
+});
+
 module.exports = router;
