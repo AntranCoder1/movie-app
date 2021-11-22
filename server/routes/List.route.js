8851edb07694dcba96f8b0a4e40c53a26bf83027
thanhantran1 <thanhantran21@gmail.com>
2021-11-22 14:28:39 +0700

    Add function delete list on server side

--- server/routes/List.route.js
+++ server/routes/List.route.js
@@ -20,6 +20,21 @@ router.post("/", verify, async (req, res) => {
     }
 });
 
+// @router api/lists/:id
+// @desc DELETE list
+// @access Private
+router.delete('/:id', verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id);
+            res.status(200).json('The list has been delete...');
+        } catch (error) {
+            res.status(500).json({ success: false, message: 'Internal server error' });
+        }
+    } else {
+        res.status(403).json({ success: false, message: 'You are not allowed!' });
+    }
+});
 
 
 
