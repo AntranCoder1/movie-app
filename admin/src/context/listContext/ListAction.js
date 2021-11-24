edff0de740194015ebd9bc4f75b129c937f30500
thanhantran1 <thanhantran21@gmail.com>
2021-11-24 18:28:30 +0700

    Create ListContext APi on admin

---
+++ admin/src/context/listContext/ListAction.js
@@ -0,0 +1,51 @@
+export const getListStart = () => ({
+    type: 'GET_LISTS_START',
+});
+
+export const getListsSuccess = (lists) => ({
+    type: 'GET_LISTS_SUCCESS',
+    payload: lists
+});
+
+export const getListsFailure = () => ({
+    type: 'GET_LISTS_FAILURE',
+});
+
+export const createListsStart = () => ({
+    type: 'GET_LISTS_START',
+});
+
+export const createListsSuccess = (list) => ({
+    type: 'CREATE_LISTS_SUCCESS',
+    payload: list
+});
+
+export const createListsFailure = () => ({
+    type: 'CREATE_LISTS_FAILURE',
+});
+
+export const updateListsStart = () => ({
+    type: 'UPDATE_LISTS_START',
+});
+
+export const updateListsSuccess = (movie) =>({
+    type: 'UPDATE_LISTS_SUCCESS',
+    payload: movie
+});
+
+export const updateListsFailure = () => ({
+    type: 'UPDATE_LISTS_FAILURE',
+});
+
+export const deleteListStart = () => ({
+    type: 'DELETE_LISTS_START'
+});
+
+export const deleteListsSuccess = (id) => ({
+    type: 'DELETE_LISTS_SUCCESS',
+    payload: id
+});
+
+export const deleteListsFailure = () => ({
+    type: 'DELETE_LISTS_FAILURE'
+});
---
+++ admin/src/pages/listList/ListList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ListList.css';
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+const ListList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListList
